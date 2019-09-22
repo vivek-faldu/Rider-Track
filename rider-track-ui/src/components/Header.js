81f318b161493622e21dd249ceabc3450cbe5336
Shaunak Shah <sms228@gmail.com>
2019-09-22 15:32:45 -0700

    US#30 Task#44 Added styling for the header. And some short cut keys.

--- rider-track-ui/src/components/Header.js
+++ rider-track-ui/src/components/Header.js
@@ -1,36 +1,50 @@
 /**
  * Author: Shaunak Shah
- * Task: Create common header for all the pages.
+ * Task: Create styles for the header for better interface.
  * Task no: 44
- * Date: 09/20/2019
+ * Date: 09/22/2019
  */
 
 import React from 'react';
+import './Header.css';
 
 import {AppBar,Button,IconButton,Toolbar} from "@material-ui/core";
 import Grid from '@material-ui/core/Grid';
+import Person from '@material-ui/icons/Person';
+import AddBox from '@material-ui/icons/AddBox';
+import Room from '@material-ui/icons/Room';
 
 class Header extends React.Component {
     render() {
         return (
-            <div className="row">
-                <AppBar position="static">
-                    <Toolbar>
-                        <Grid direction="row" container spacing={2}>
-                            <Grid item justify="center">
-                                <IconButton edge="start" color="inherit" aria-label="menu">
-                                    Rider Track
-                                </IconButton>
-                            </Grid>
-                            <Grid item justify="flex-end" xs={12} sm container>
-                                <Button color="inherit">Home</Button>
-                                <Button color="inherit">Organizers</Button>
-                                <Button color="inherit">About</Button>
-                                <Button color="inherit">Login</Button>
-                            </Grid>
+            <div>
+
+                <Grid container alignItems="center" className="country_bar">
+                    <Grid item lg={10}></Grid>
+                    <Grid intem lg={2}><p>United States</p></Grid>
+                </Grid>
+
+                <Grid container alignItems="center" className="nav_bar">
+                        <Grid xs={2} item>
+                            <Button color="inherit" className="home_button">Rider Track</Button>
                         </Grid>
-                    </Toolbar>
-                </AppBar>
+                        <Grid item xs={8}r>
+                            <Button className="menu_button" color="inherit">Home</Button>
+                            <Button className="menu_button" color="inherit">Organizers</Button>
+                            <Button className="menu_button" color="inherit">About</Button>
+                            <Button className="menu_button" color="inherit">Login</Button>
+                        </Grid>
+                        <Grid container alignment="center" justify="center" xs={2} spacing={3}>
+                            <Grid item>{<Person />}</Grid>
+                            <Grid item>{<AddBox />}</Grid>
+                            <Grid item>{<Room />}</Grid>
+                        </Grid>
+                </Grid>
+                <Grid container alignItems="center" className="info_bar">
+                    <Grid item xs={12}>
+                        <p>ORGANIZE, PARTICIPATE, TRACK AND SHARE EVENTS! VISUALIZE THE EVENTS AS THEY TAKE PLACE</p>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
