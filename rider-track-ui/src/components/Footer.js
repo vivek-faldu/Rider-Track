aad5545ac5393fbbe26e5263bbde0b8bbed308be
Shaunak Shah <sms228@gmail.com>
2019-09-22 19:44:10 -0700

    US#30 Task#44 Add style to footer and make it responsive for small screens.

--- rider-track-ui/src/components/Footer.js
+++ rider-track-ui/src/components/Footer.js
@@ -1,35 +1,48 @@
 /**
  * Author: Shaunak Shah
- * Task: Create common footer for all the pages.
+ * Task: Add style to the footer and compatibility for small screens.
  * Task no: 44
- * Date: 09/20/2019
+ * Date: 09/22/2019
  */
 
 import React from 'react';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import FolderIcon from '@material-ui/icons/Folder';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
-
+import Grid from '@material-ui/core/Grid';
+import './Footer.css'
+import {Button} from "@material-ui/core";
+import Hidden from '@material-ui/core/Hidden';
 
 class Footer extends React.Component {
     render(){
-        const bottom = {
-            position: 'absolute',
-            bottom: "0",
-            backgroundColor: "#3f51b5",
-            width: "100%",
-        };
 
         return (
-            <BottomNavigation color="inherit" style={bottom}>
-                <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-                <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-                <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-                <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-            </BottomNavigation>
+            <Grid container alignItems="center" className="bottom">
+                <Hidden mdDown>
+                    <Grid container item md={4} direction="column" justify="center" alignItems="center">
+                        <Grid item md={3}><Button className="footer_button" color="inherit">Home</Button></Grid>
+                        <Grid item md={3}><Button className="footer_button" color="inherit">ORGANIZERS</Button></Grid>
+                        <Grid item md={3}><Button className="footer_button" color="inherit">ABOUT</Button></Grid>
+                        <Grid item md={3}><Button className="footer_button" color="inherit">LOGIN</Button></Grid>
+                    </Grid>
+                </Hidden>
+                <Grid item md={4} container direction="row" justify="space-between" alignItems="center">
+                    <Grid  md={3} className="quick_actions"><FolderIcon /></Grid>
+                    <Grid  md={3} className="quick_actions"><RestoreIcon /></Grid>
+                    <Grid  md={3} className="quick_actions"><FavoriteIcon /></Grid>
+                    <Grid  md={3} className="quick_actions"><LocationOnIcon /></Grid>
+                </Grid>
+                <Hidden mdDown>
+                    <Grid item md={4} container direction="column" justify="center" alignItems="center">
+                        <Grid item><Button className="footer_button" color="inherit">Facebook</Button></Grid>
+                        <Grid item><Button className="footer_button" color="inherit">Twitter</Button></Grid>
+                        <Grid item><Button className="footer_button" color="inherit">Instgram</Button></Grid>
+                        <Grid item><Button className="footer_button" color="inherit">Youtube</Button></Grid>
+                    </Grid>
+                </Hidden>
+            </Grid>
         );
     }
 }
