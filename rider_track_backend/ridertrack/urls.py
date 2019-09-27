f54f4d5e86f7613801d455ac12d4738bdb60c6d4
shipabhat92 <sgbhat2@asu.edu>
2019-09-26 19:05:42 -0700

    Change Type:  Refactor Summary: Remove python project (unused) Test Plan: Manual Possible Regressions: None

--- rider_track_backend/ridertrack/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import EventDetailsView, EventView
-
-urlpatterns = [
-    path('api/event-details/{eventId}', EventDetailsView.as_view(), name="event-details"),
-    path('api/events/', EventView.as_view(), name="events"),
-]
