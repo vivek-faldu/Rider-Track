61a02512f8db3a1d0f9d2a1dc72d08f37217f229
jananit2411 <jthiaga1@asu.edu>
2019-10-04 16:13:52 -0700

    Ticket: US#3, Task#64
    Change Type:  Feature
    Summary: Added code to fetch participant history
    Test Plan: None
    Possible Regressions: None

--- rider-track-backend/routes/user.js
+++ rider-track-backend/routes/user.js
@@ -6,16 +6,36 @@
  */
 var express = require('express');
 var router = express.Router();
+
 const Users = require('../models/users');
+const Event = require('../models/events');
 
 router.get("/:id/events", async (req, res) => {
+    var pList = [];
+    var eventList = [];
 
-    Users.findById(req.params.id, (err, user) => {
+    var pList = Users.findById(req.params.id, function (err, user, test) {
         if (err) {
             console.log(err);
-        } else
-            console.log(user);
-        res.json(user);
-    });
+        } else {
+            pList = user.participated_events;
+            res.status(200).json(pList);
+            /* for (var i = 0; i < pList.length; i++) {
+                Event.findById(pList[i], function (err, event) {
+                    var item = new Event();
+                    if (err) {
+                        console.log("Error Occured");
+                        //res.status(500).json({'message':'Error while fecthing user history'});
+                    } else {
+                        item.event_name = event.event_name;
+                        item.date_time = event.date_time;
+                        item.duration = event.duration;
+                        eventList.push(item);
+                    }
+                });
+            } */
+        }
+    })
+
 });
 module.exports = router;
