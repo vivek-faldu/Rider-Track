92508cf85d376b74a44ed5bd34235f493d39dc88
Vivek Vinodchandra Faldu <vfaldu@asu.edu>
2019-10-22 21:25:47 -0700

    Title: Cutomised Live Event Page with tab view Ticket: US#105 Task#106 Change Type: Feature Test Plan: Manual Possible Regressions: None

--- rider-track-ui/src/components/live/LivePage.js
+++ rider-track-ui/src/components/live/LivePage.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { Container } from '@material-ui/core';
-import LiveEventMap from './LiveEventMap';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
+import TabView from './TabView';
+import PaticipantList from './ParticipantList';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class LivePage extends Component {
     render() {
         return (
-          <Container>
-            <LiveEventMap />
-          </Container>
+          <div>
+
+            <Grid container spacing={1}>
+
+              <Grid item xs={3}>
+                <Paper style={{ height: 50 }}>
+                  <Typography variant="h6" color="textSecondary">Arizona Riders Association</Typography>
+                </Paper>
+                <PaticipantList />
+              </Grid>
+              <Grid item xs={9}>
+                <TabView />
+              </Grid>
+            </Grid>
+          </div>
         );
     }
 }
