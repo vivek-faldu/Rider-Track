efe7b11a16f471dfd11339d9fbe9b4a434890200
shipabhat92 <sgbhat2@asu.edu>
2019-10-22 18:19:47 -0700

    User story:98 Ticket: #111 Change Type: Fix Summary: Add event status Test Plan: Manual Possible Regressions: None

--- rider-track-backend/services/eventManager.js
+++ rider-track-backend/services/eventManager.js
@@ -16,23 +16,27 @@ function startEvent(req, res) {
         if (err) {
             return err;
         }
-        participants = new Array();
-        participants = event.participants;
-        if (participants.length > 0) {
-            participants.forEach(participant => {
-                User.findById(participant.id, function (err, user) {
-                    user.live_event = req.params.id;
-                    user.save().then(() => {
-                        res.status(200).json({
-                            status: 200,
-                            event: "Started successfully"
-                        });
-                    }).catch(() => {
-                        res.status(400).send("Failed to Start the event");
+        event.status = 'Live';
+        event.save().then(() => {
+            participants = new Array();
+            participants = event.participants;
+            if (participants.length > 0) {
+                participants.forEach(participant => {
+                    User.findById(participant.id, function (err, user) {
+                        user.live_event = req.params.id;
+                        user.save().then(() => {
+                            res.status(200).json({
+                                status: 200,
+                                event: "Started successfully"
+                            });
+                        }).catch((err) => {
+                            throw err;
+                            // res.status(400).send("Failed to Start the event");
+                        })
                     })
-                })
-            });
-        }
+                });
+            }
+        })
     })
 }
 
