b36e7941a245df4b85f75a6b9a5f7502f9800e1b
jananit2411 <jthiaga1@asu.edu>
2019-11-05 14:31:57 -0700

    Ticket: US#124 Task#149
    Change Type:  Feature
    Summary: Add Backend API to update details related to specific user
    Test Plan: Unit Test

--- rider-track-backend/test/user.js
+++ rider-track-backend/test/user.js
@@ -8,6 +8,11 @@
  * Task no: 83
  * Date: 10/07/2019
  * 
+ * 
+ * Task: Unit tests for api call to update user profile
+ * Task no: 149
+ * Date: 11/05/2019
+ * 
  */
 let mongoose = require("mongoose");
 let Event = require('../models/events');
@@ -39,7 +44,7 @@ describe('GET List of Events for a participant', () => {
 describe('GET List of Checkpoints for an event a participant took part in', () => {
     it('it should GET all the checkpoints and event details for an event', (done) => {
         chai.request(server)
-            .get('/api/user/eventdetail?eventid=5da0111bf05a835eba01381d&userid=5d96e4e1e78f0b615d85cf34')
+            .get('/api/user/eventdetail?eventid=5db26498b3cbfb4946b135db&userid=5db266c7f77c0a1728f5e2c4')
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.have.property('event_name');
@@ -47,9 +52,25 @@ describe('GET List of Checkpoints for an event a participant took part in', () =
                 res.body.should.have.property('date_time');
                 res.body.should.have.property('duration');
                 res.body.should.have.property('checkpoints');
-                res.body['checkpoints'][0].should.have.property('lat');
-                res.body['checkpoints'][0].should.have.property('long');
+                res.body['checkpoints'][0].should.have.property('latitude');
+                res.body['checkpoints'][0].should.have.property('longitude');
                 res.body['checkpoints'][0].should.have.property('timestamp');
+                done();
+            });
+    });
+});
+
+describe('PUT User Profile', () => {
+    it('it should save details related to a user profile', (done) => {
+        let user = {
+            "email": "TestUser1@test.com",
+            "username": "Test User 1"
+        }
+        chai.request(server)
+            .put('/api/user/profile/5dc0fd9ac31f236658ab1b51')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200);
                 done();
             });
     });
