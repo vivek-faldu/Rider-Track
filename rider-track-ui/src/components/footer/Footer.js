70d9e632dcfb1fffa437da8bc64c3c68a69b0443
saran1856 <skandim2@asu.edu>
2019-11-13 20:07:28 -0700

    added login and logout buttons and state to the footer. US: #173, Task: #175

--- rider-track-ui/src/components/footer/Footer.js
+++ rider-track-ui/src/components/footer/Footer.js
@@ -5,7 +5,8 @@
  * Date: 09/26/2019
  */
 
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import FolderIcon from '@material-ui/icons/Folder';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -14,34 +15,80 @@ import Grid from '@material-ui/core/Grid';
 import './Footer.css';
 import { Button } from '@material-ui/core';
 import Hidden from '@material-ui/core/Hidden';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Login from '../authentication/Login';
+import Logout from '../authentication/Logout';
 
-function Footer() {
-  return (
-    <Grid container alignItems="center" className="bottom">
-      <Hidden mdDown>
-        <Grid container item md={4} direction="column" justify="center" alignItems="center">
-          <Grid item md={3}><Button className="footer_button" color="inherit">Home</Button></Grid>
-          <Grid item md={3}><Button className="footer_button" color="inherit">Organisers</Button></Grid>
-          <Grid item md={3}><Button className="footer_button" color="inherit">About</Button></Grid>
-          <Grid item md={3}><Button className="footer_button" color="inherit">Login</Button></Grid>
+class Footer extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false,
+    }
+  }
+
+  UNSAFE_componentWillReceiveProps = (newProps) => {
+    if(newProps.authentication) {
+      this.setState({
+        isLoggedIn: newProps.authentication.isAuthenticated,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Grid container alignItems="center" className="bottom">
+        <Hidden mdDown>
+          <Grid container item md={4} direction="column" justify="center" alignItems="center">
+            <Grid item md={3}><Button className="footer_button" color="inherit">Home</Button></Grid>
+            <Grid item md={3}><Button className="footer_button" color="inherit">Organisers</Button></Grid>
+            <Grid item md={3}><Button className="footer_button" color="inherit">About</Button></Grid>
+            { this.state.isLoggedIn?
+            (
+              <Grid item md={3}>
+                <Button className="footer_button" color="inherit">
+                  <Logout />
+                </Button>
+              </Grid>
+            ):(
+              <Grid item md={3}>
+                <Button className="footer_button" color="inherit">
+                  <Login />
+                </Button>
+              </Grid>
+            )}
+          </Grid>
+        </Hidden>
+        <Grid item md={4} container direction="row" justify="space-between" alignItems="center">
+          <Grid md={3} className="quick_actions"><FolderIcon /></Grid>
+          <Grid md={3} className="quick_actions"><RestoreIcon /></Grid>
+          <Grid md={3} className="quick_actions"><FavoriteIcon /></Grid>
+          <Grid md={3} className="quick_actions"><LocationOnIcon /></Grid>
         </Grid>
-      </Hidden>
-      <Grid item md={4} container direction="row" justify="space-between" alignItems="center">
-        <Grid md={3} className="quick_actions"><FolderIcon /></Grid>
-        <Grid md={3} className="quick_actions"><RestoreIcon /></Grid>
-        <Grid md={3} className="quick_actions"><FavoriteIcon /></Grid>
-        <Grid md={3} className="quick_actions"><LocationOnIcon /></Grid>
+        <Hidden mdDown>
+          <Grid item md={4} container direction="column" justify="center" alignItems="center">
+            <Grid item><Button className="footer_button" color="inherit">Facebook</Button></Grid>
+            <Grid item><Button className="footer_button" color="inherit">Twitter</Button></Grid>
+            <Grid item><Button className="footer_button" color="inherit">Instgram</Button></Grid>
+            <Grid item><Button className="footer_button" color="inherit">Youtube</Button></Grid>
+          </Grid>
+        </Hidden>
       </Grid>
-      <Hidden mdDown>
-        <Grid item md={4} container direction="column" justify="center" alignItems="center">
-          <Grid item><Button className="footer_button" color="inherit">Facebook</Button></Grid>
-          <Grid item><Button className="footer_button" color="inherit">Twitter</Button></Grid>
-          <Grid item><Button className="footer_button" color="inherit">Instgram</Button></Grid>
-          <Grid item><Button className="footer_button" color="inherit">Youtube</Button></Grid>
-        </Grid>
-      </Hidden>
-    </Grid>
-  );
+    );
+  }
 }
 
-export default Footer;
+Footer.PropTypes = {
+  authentication: PropTypes.object.isRequired,
+};
+
+const mapState = (state) => ({
+  authentication: state.authentication,
+  errors: state.errors,
+});
+
+export default connect(
+  mapState,
+)(withRouter(Footer));
