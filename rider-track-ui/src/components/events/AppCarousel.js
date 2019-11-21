0a6ea8a324e5daf108b0d0b5fb90dc6ece45b44f
saran1856 <skandim2@asu.edu>
2019-11-20 20:44:12 -0700

    added background images for carousel. Task: #185

--- rider-track-ui/src/components/events/AppCarousel.js
+++ rider-track-ui/src/components/events/AppCarousel.js
@@ -9,47 +9,39 @@ import Carousel from 'react-bootstrap/Carousel';
  * Referred from: https://react-bootstrap.github.io/components/carousel/
  */
 class AppCarousel extends Component {
-  render() {
-    return (
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="http://americanpronunciationcoach.com/wp-content/uploads/1200-x-300-website-sliders-china-man.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>Participante in events</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://cdn5.identityforce.com/wp-content/uploads/2017/06/header-support-1280x570-1-1200x300.jpg"
-            alt="Third slide"
-          />
 
-          <Carousel.Caption>
-            <h3>Live Track events</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://sd41.org/wp-content/uploads/2015/09/006-3-1200x300.jpg"
-            alt="Third slide"
-          />
+    render() {
+        return (
+            <Carousel className="carousel-rt">
+                <Carousel.Item className="rt-carousel-img-1">
+                    <div className="d-block w-100">
+                        <p>Slide 1</p>
+                        <Carousel.Caption>
+                            <h3>Participate in events</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item className="rt-carousel-img-2">
+                    <div className="d-block w-100">
 
-          <Carousel.Caption>
-            <h3>Create Events</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
-  }
+                        <Carousel.Caption>
+                            <h3>Track Live events</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item className="rt-carousel-img-1">
+                    <div className="d-block w-100">
+                        <Carousel.Caption>
+                            <h3>Create Events</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </div>
+                </Carousel.Item>
+            </Carousel>
+        );
+    }
 }
 
 export default AppCarousel;
